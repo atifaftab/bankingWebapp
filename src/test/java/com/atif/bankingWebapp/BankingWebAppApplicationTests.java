9dfd040156d308f8b0a8db6bd9a1d28c445a2fa2
Atif <iamatif96@gmail.com>
2023-05-18 17:48:50 +0800

    Initial commit

---
+++ src/test/java/com/atif/bankingWebapp/BankingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atif.bankingWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
